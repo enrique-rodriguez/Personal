03b8d98c5ce2b965f01b5c3c806eb935ab3ebcdf
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2021-07-18 09:19:18 -0400

    Update settings

--- web/settings.py
+++ web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SITE_ID = 1
+
+django_heroku.settings(locals())
